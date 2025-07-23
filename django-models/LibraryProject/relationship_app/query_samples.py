f0e4ed501b988123e7f118f514c328e81fbb0cf5
nora968-lkj <nora6185g@gmail.com>
2025-07-23 04:13:48 +0300

    Initial commit for django-models

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -3,7 +3,7 @@ from relationship_app.models import Author, Book, Library, Librarian
 # Query all books by a specific author
 def list_books_by_author(author_name):
     author = Author.objects.get(name=author_name)
-    books = author.books.all()
+    books = Book.objects.filter(author=author)
     print("📚 كل الكتب لمؤلف محدد:")
     for book in books:
         print(f"- {book.title}")
