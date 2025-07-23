759473bd8d6203ad25c63e21602df1bf53106f3b
nora968-lkj <nora6185g@gmail.com>
2025-07-23 05:19:06 +0300

    Initial commit for django-models

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,13 +1,13 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render
 from django.views.generic import DetailView
 from .models import Book, Library
 
-# Function-based view: List all books
+# Function-based view: عرض كل الكتب
 def list_books(request):
     books = Book.objects.all()
     return render(request, 'relationship_app/list_books.html', {'books': books})
 
-# Class-based view: Library detail
+# Class-based view: عرض تفاصيل مكتبة محددة وكل الكتب فيها
 class LibraryDetailView(DetailView):
     model = Library
     template_name = 'relationship_app/library_detail.html'
